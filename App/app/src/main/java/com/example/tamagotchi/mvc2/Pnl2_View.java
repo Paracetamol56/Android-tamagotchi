49e0a816ec314564caf01a9f23e79b1585c9edcb
lechiffre4 <toxcoa@gmail.com>
2021-06-08 20:42:34 +0200

    Image Changing depending on state

--- App/app/src/main/java/com/example/tamagotchi/mvc2/Pnl2_View.java
+++ App/app/src/main/java/com/example/tamagotchi/mvc2/Pnl2_View.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -20,14 +21,16 @@ public class Pnl2_View extends LinearLayout implements Observer {
 
     public TextView txtHeader;
     public TextView txtMessage;
-    public TextView txtHunger;
-    public TextView txtHappiness;
+    public TextView txtFaim;
+    public TextView txtBonheur;
 
     public Pnl2_Mdl refMdl;
     public Pnl2_Ctrl refCtrl;
 
     public Button btnGotoPnl1;
     public Button btnGotoPnl3;
+
+    public ImageView State;
 
     public Pnl2_View(Context context) {
         super(context);
@@ -40,12 +43,14 @@ public class Pnl2_View extends LinearLayout implements Observer {
         btnNourrir = (Button)findViewById(R.id.btnNourrir);
 
         txtHeader = (TextView)findViewById(R.id.txtHeader);
-        txtHunger = (TextView)findViewById(R.id.txtHunger);
-        txtHappiness = (TextView)findViewById(R.id.txtHappiness);
+        txtFaim = (TextView)findViewById(R.id.txtHunger);
+        txtBonheur = (TextView)findViewById(R.id.txtHappiness);
         txtMessage = (TextView)findViewById(R.id.txtMessage);
 
         btnGotoPnl1 = (Button)findViewById(R.id.btnGotoPnl1);
         btnGotoPnl3 = (Button)findViewById(R.id.btnGotoPnl3);
+
+        State =(ImageView)findViewById(R.id.State);
     }
 
     public void setRefCtrl(Pnl2_Ctrl c) {
@@ -62,22 +67,29 @@ public class Pnl2_View extends LinearLayout implements Observer {
     public void update(Observable observable, Object o) {
         txtHeader.setText("Mes chaussettes sont sales");
 
-        txtHunger.setText(Integer.toString(refMdl.getHunger()));
-        txtHappiness.setText(Integer.toString(refMdl.getHappiness()));
+        Log.i("yo", findViewById(R.id.txtHunger).toString());
+        Log.i("yo", findViewById(R.id.txtHunger).toString());
+
+        txtFaim.setText(Integer.toString(refMdl.getHunger()));
+        txtBonheur.setText(Integer.toString(refMdl.getHappiness()));
 
         switch (refMdl.getMood())
         {
             case HAPPY:
                 txtMessage.setText("Content");
+                State.setImageResource(R.drawable.heureux);
                 break;
             case SAD:
                 txtMessage.setText("Triste");
+                State.setImageResource(R.drawable.triste);
                 break;
             case HUNGRY:
                 txtMessage.setText("Trop faim");
+                State.setImageResource(R.drawable.faim);
                 break;
             case DEAD:
                 txtMessage.setText("Dead");
+                State.setImageResource(R.drawable.drogue);
                 break;
         }
     }
