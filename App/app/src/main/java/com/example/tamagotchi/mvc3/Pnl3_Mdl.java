9d5dd7c0ae676e71c79cc6d89c86ea1130ee2d01
Mathéo <matheo.galu56@gmail.com>
2021-06-08 22:55:16 +0200

    3rd screen text edit

--- App/app/src/main/java/com/example/tamagotchi/mvc3/Pnl3_Mdl.java
+++ App/app/src/main/java/com/example/tamagotchi/mvc3/Pnl3_Mdl.java
@@ -1,9 +1,31 @@
 package com.example.tamagotchi.mvc3;
 
+import android.widget.EditText;
+
 import java.util.Observable;
 
 public class Pnl3_Mdl extends Observable {
-    public Pnl3_Mdl() {
+    private String tamagotchiNameStr;
+    private String playerNameStr;
 
+    public Pnl3_Mdl() {
+        tamagotchiNameStr = "Gros CON";
+        playerNameStr = "Le boss";
+    }
+
+    public void setTamagotchiNameStr(String tamagotchiNameStr) {
+        this.tamagotchiNameStr = tamagotchiNameStr;
+    }
+
+    public void setPlayerNameStr(String playerNameStr) {
+        this.playerNameStr = playerNameStr;
+    }
+
+    public String getTamagotchiNameStr() {
+        return tamagotchiNameStr;
+    }
+
+    public String getPlayerNameStr() {
+        return playerNameStr;
     }
 }
--- App/app/src/main/java/com/example/tamagotchi/mvc3/Pnl3_View.java
+++ App/app/src/main/java/com/example/tamagotchi/mvc3/Pnl3_View.java
@@ -4,14 +4,17 @@ import android.app.Activity;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 
 import com.example.tamagotchi.R;
 
 public class Pnl3_View extends LinearLayout {
     public Pnl3_Ctrl refCtrl;
-
     public Button btnGotoPnl2;
+
+    private EditText txtTamagotchiName;
+    private EditText txtPlayerName;
 
     public Pnl3_View(Context context) {
         super(context);
@@ -20,6 +23,9 @@ public class Pnl3_View extends LinearLayout {
         inflater.inflate(R.layout.layout_pnl3, this);
 
         btnGotoPnl2= (Button)findViewById(R.id.btnGotoPnl1);
+
+        txtTamagotchiName = (EditText)findViewById(R.id.inputTamagotchiName);
+        txtTamagotchiName = (EditText)findViewById(R.id.inputPlayerName);
     }
 
     public void setRefCtrl(Pnl3_Ctrl c) {
