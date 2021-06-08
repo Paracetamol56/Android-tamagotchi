e5a3492603e9216b5740b8cd118f607c4a1e22bb
lechiffre4 <toxcoa@gmail.com>
2021-06-08 22:30:29 +0200

    Random image in Panel1

--- App/app/src/main/java/com/example/tamagotchi/mvc1/Pnl1_View.java
+++ App/app/src/main/java/com/example/tamagotchi/mvc1/Pnl1_View.java
@@ -4,15 +4,23 @@ import android.app.Activity;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import com.example.tamagotchi.R;
+
+import java.util.Random;
+
 
 public class Pnl1_View extends LinearLayout {
     public Pnl1_Ctrl refCtrl;
 
     public Button btnGotoPnl2;
     public Button btnGotoPnl3;
+
+    public ImageView Image;
+
+
 
     public Pnl1_View(Context context) {
         super(context);
@@ -22,6 +30,29 @@ public class Pnl1_View extends LinearLayout {
 
         btnGotoPnl2= (Button)findViewById(R.id.btnGotoPnl1);
         btnGotoPnl3= (Button)findViewById(R.id.btnGotoPnl3);
+
+        Image=(ImageView)findViewById(R.id.MainImage);
+
+        //Random pics
+        final int random = new Random().nextInt(4);
+
+        switch (random)
+        {
+            case 0 :
+                Image.setImageResource(R.drawable.heureux);
+                break;
+            case 1 :
+                Image.setImageResource(R.drawable.triste);
+                break;
+            case 2 :
+                Image.setImageResource(R.drawable.faim);
+                break;
+            case 3 :
+                Image.setImageResource(R.drawable.drogue);
+                break;
+        }
+
+
     }
 
     public void setRefCtrl(Pnl1_Ctrl c) {
