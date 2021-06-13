e0557b832874db081b63456af237433a2aea2250
Mathéo <matheo.galu56@gmail.com>
2021-06-13 17:06:52 +0200

    Comments

--- App/app/src/main/java/com/example/tamagotchi/mvc2/Pnl2_Mdl.java
+++ App/app/src/main/java/com/example/tamagotchi/mvc2/Pnl2_Mdl.java
@@ -1,3 +1,12 @@
+/*
+ * Created on Sun Jun 13 2021
+ *
+ * Copyright (c) 2021 - Mathéo G & Sahel H - All Right Reserved
+ *
+ * Licensed under the Apache License, Version 2.0
+ * Available on GitHub at https://github.com/Paracetamol56/Android-tamagotchi
+ */
+
 package com.example.tamagotchi.mvc2;
 
 import android.os.Handler;
